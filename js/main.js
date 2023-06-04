2f8a4797cb9fbb3100d7c884976ea3b965dae0cb
Joyce Lemos <joyce.lemoss@gmail.com>
2023-06-04 19:11:26 -0300

    :zap: Update: Enhance form interaction and introduce task completion toggle

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
     $('header button').click(function() {
-        $('form').slideDown();
-    })
+        $('form').slideToggle();
+    });
 
     $('#cancel-button').click(function() {
         $('form').slideUp();
-    })
+    });
 
     $('form').on('submit', function(e){
         e.preventDefault();
 
         const newTaskInput = $('#new_task').val();
-        const new_task = $('<li></li>');
-        
-        $(`<li>${newTaskInput}</li>`).appendTo(new_task);
+        const new_task = $('<li></li>').text(newTaskInput);
+
         $(new_task).appendTo('ul');
         $(new_task).fadeIn(1000);
-        $('#new_task').val('')
+        $('#new_task').val('');
     })
+
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
 })
