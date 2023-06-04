1947798a1338f6d50bef0279f23e6f3d26bc323e
Joyce Lemos <joyce.lemoss@gmail.com>
2023-06-04 19:42:10 -0300

    :sparkles: New Feature: Implement localStorage to persist added tasks upon page refresh

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+
+    const storedTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    storedTasks.forEach(function(taskText) {
+        createTaskElement(taskText);
+    });    
+
     $('header button').click(function() {
         $('form').slideToggle();
     });
@@ -11,14 +17,31 @@ $(document).ready(function() {
         e.preventDefault();
 
         const newTaskInput = $('#new_task').val();
-        const new_task = $('<li></li>').text(newTaskInput);
+        createTaskElement(newTaskInput);
 
-        $(new_task).appendTo('ul');
-        $(new_task).fadeIn(1000);
         $('#new_task').val('');
-    })
+    });
 
     $('ul').on('click', 'li', function() {
         $(this).toggleClass('completed');
+        updateLocalStorage();
     });
-})
+
+    function createTaskElement(taskText) {
+        const new_task = $('<li></li>').text(taskText);
+
+        $(new_task).appendTo('ul');
+        $(new_task).fadeIn(1000);
+
+        updateLocalStorage();
+    }
+
+    function updateLocalStorage() {
+        const tasks = [];
+        $('ul li').each(function() {
+            tasks.push($(this).text());
+        });
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+});
